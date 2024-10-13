e33961734b7402d76120a06153cba381a5b772a5
Albert Du <52804499+albert-du@users.noreply.github.com>
2024-10-13 10:11:59 -0700

    Update cloudflare-worker.js

--- cloudflare-worker.js
+++ cloudflare-worker.js
@@ -1,15 +1,25 @@
-export default {
+const corsHeaders = {
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Methods': 'GET',
+    'Access-Control-Allow-Origin': '*',
+  }
+  
+  export default {
     async fetch(request, env) {
-        const url = new URL(request.url);
-        const p = url.searchParams.get('prompt');
-        const tasks = [];
-
-        let simple = {
-            prompt: p
-        };
-        let response = await env.AI.run('@cf/meta/llama-3-8b-instruct', simple);
-        tasks.push({ inputs: simple, response });
-
-        return Response.json(tasks);
+      const url = new URL(request.url);
+      const p = url.searchParams.get('prompt');
+      const tasks = [];
+  
+      let simple = {
+        prompt: p
+      };
+      let response = await env.AI.run('@cf/meta/llama-3-8b-instruct', simple);
+      tasks.push({ inputs: simple, response });
+  
+      return Response.json(tasks,{
+        headers: {
+          'Content-type': 'application/json',
+          ...corsHeaders //uses the spread operator to include the CORS headers.
+        }});
     }
-};
+  };
